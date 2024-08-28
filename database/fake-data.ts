e8d641326e616877529455087c3562004717fc68
thaimeo03 <thaitran15072003@gmail.com>
2024-08-28 16:56:19 +0700

    Update: routes and routeStops seed data

--- database/fake-data.ts
+++ database/fake-data.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { RouteStop } from 'src/route-stops/entities/route-stop.entity'
 import { Route } from 'src/routes/entities/route.entity'
@@ -8,6 +8,8 @@ import { fakerVI } from '@faker-js/faker'
 
 @Injectable()
 export class DataService {
+  private logger: Logger = new Logger(DataService.name)
+
   constructor(
     @InjectRepository(Route) private routeRepository: Repository<Route>,
     @InjectRepository(Schedule) private scheduleRepository: Repository<Schedule>,
@@ -15,58 +17,14 @@ export class DataService {
   ) {}
 
   async seedData() {
-    // seed routes
-    const routes: Route[] = this.seedRoutes(2)
+    this.logger.warn('Seeding data...')
 
-    // seed route stops
-    // const routeStops: RouteStop[] = [
-    //   // start location
-    //   this.routeStopRepository.create({
-    //     route: routes[0],
-    //     location: 'Sân bay Nội Bài',
-    //     arrivalTime: faker.date.past(),
-    //     distanceFromStartKm: 0
-    //   }),
+    // const routes: Route[] = await this.routeRepository.save(this.seedRoutes(2))
+    // const routeStops: RouteStop[] = await this.routeStopRepository.save(
+    //   this.seedRouteStops({ quantityInBetween: 1, routes: routes })
+    // )
 
-    //   // middle location
-    //   this.routeStopRepository.create({
-    //     route: routes[0],
-    //     location: '4 Thọ Tháp',
-    //     arrivalTime: faker.date.recent(),
-    //     distanceFromStartKm: 5
-    //   }),
-
-    //   this.routeStopRepository.create({
-    //     route: routes[0],
-    //     location: '43 Nguyễn Quốc Trí',
-    //     arrivalTime: faker.date.recent(),
-    //     distanceFromStartKm: 4
-    //   }),
-
-    //   // end location
-    //   this.routeStopRepository.create({
-    //     route: routes[0],
-    //     location: '08A Vincom Shophouse Thái Bình',
-    //     arrivalTime: faker.date.recent(),
-    //     distanceFromStartKm: 100
-    //   })
-    // ]
-
-    // seed schedules
-    // const schedules: Schedule[] = [
-    //   this.scheduleRepository.create({
-    //     routeStop: routeStops[0],
-    //     departureTime: faker.date.past()
-    //   }),
-    //   this.scheduleRepository.create({
-    //     routeStop: routeStops[0],
-    //     departureTime: faker.date.past()
-    //   })
-    // ]
-
-    await this.routeRepository.save(routes)
-    // await this.routeStopRepository.save(routeStops)
-    // await this.scheduleRepository.save(schedules)
+    this.logger.warn('Seeding data complete!')
   }
 
   seedRoutes(quantity: number): Route[] {
@@ -97,5 +55,62 @@ export class DataService {
     }
 
     return routes
+  }
+
+  seedRouteStops({ quantityInBetween, routes }: { quantityInBetween: number; routes: Route[] }): RouteStop[] {
+    const routeStops: RouteStop[] = []
+
+    for (const route of routes) {
+      // Start location
+      const startLocationDeparture = fakerVI.location.streetAddress() + ', ' + route.startLocation
+      const arrivalTimeStartDeparture = fakerVI.date.future()
+      routeStops.push(
+        this.routeStopRepository.create({
+          route: route,
+          location: startLocationDeparture,
+          arrivalTime: arrivalTimeStartDeparture,
+          distanceFromStartKm: 0
+        })
+      )
+
+      // Middle locations
+      const middleLocations: string[] = []
+      let quantity = quantityInBetween,
+        countTime = 1,
+        distance = quantityInBetween
+
+      while (quantity--) {
+        const middleLocation = fakerVI.location.streetAddress() + ', ' + fakerVI.location.city()
+        const moreDistance = fakerVI.number.int({ min: 1, max: 10 })
+
+        middleLocations.push(middleLocation)
+
+        routeStops.push(
+          this.routeStopRepository.create({
+            route: route,
+            location: middleLocation,
+            arrivalTime: new Date(arrivalTimeStartDeparture.getTime() + 60 * 60 * 1000 * countTime++),
+            distanceFromStartKm: distance + moreDistance
+          })
+        )
+
+        distance += moreDistance
+      }
+
+      // End location
+      const endLocationDeparture = fakerVI.location.streetAddress() + ', ' + route.endLocation
+      const arrivalTimeEndDeparture = new Date(arrivalTimeStartDeparture.getTime() + 60 * 60 * 1000 * countTime++)
+
+      routeStops.push(
+        this.routeStopRepository.create({
+          route: route,
+          location: endLocationDeparture,
+          arrivalTime: arrivalTimeEndDeparture,
+          distanceFromStartKm: route.distanceKm
+        })
+      )
+    }
+
+    return routeStops
   }
 }
