f1b8c32734f7a8a0d2627440a9d5fb87a988460f
thaimeo03 <thaitran15072003@gmail.com>
2024-09-04 08:14:48 +0700

    Update: postgres db vercel host

--- database/data-source.ts
+++ database/data-source.ts
@@ -9,6 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  url: 'postgres://default:u4fR1otxeHAy@ep-aged-mud-a1oevbq5-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require',
   database: process.env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/database/migrations/*{.ts,.js}'],
